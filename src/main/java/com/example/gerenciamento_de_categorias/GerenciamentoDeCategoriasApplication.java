af4023d8fa2199f17d6302ff50203b6dd6f876ea
Arthur L Molina <arthurlavagnollimolina@gmail.com>
2026-03-18 20:00:45 -0300

    Initial commit

---
+++ src/main/java/com/example/gerenciamento_de_categorias/GerenciamentoDeCategoriasApplication.java
@@ -0,0 +1,13 @@
+package com.example.gerenciamento_de_categorias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoDeCategoriasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoDeCategoriasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gerenciamento_de_categorias/GerenciamentoDeCategoriasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gerenciamento_de_categorias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoDeCategoriasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
